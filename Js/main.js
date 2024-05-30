a18645670634c5144a1bd93e26fb91da1bea5c20
AlvaroArizu <alvaro.arizu2000@gmail.com>
2024-05-30 17:32:25 -0300

    Agregar interaccion al NavBar Hamburguesa, cuando se pulsa una seccion se retrae con la funcion toogleMenu()

--- Js/main.js
+++ Js/main.js
@@ -2,14 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const navbarCollapse = document.getElementById('navigation');
     const mobileMenuIcon = document.querySelector('.info-icon');
 
-    mobileMenuIcon.addEventListener('click', function () {
-        if (navbarCollapse.classList.contains('show')) {
-            navbarCollapse.classList.remove('show');
-        } else {
-            navbarCollapse.classList.add('show');
-        }
+    // Función para alternar la visibilidad del menú
+    function toggleMenu() {
+        navbarCollapse.classList.toggle('show');
+    }
+
+    // Evento para controlar el clic en el icono del menú
+    mobileMenuIcon.addEventListener('click', toggleMenu);
+
+    // Añade un evento clic a cada enlace en el menú para cerrar el menú después de la selección
+    document.querySelectorAll('#navigation .nav-link').forEach(function (link) {
+        link.addEventListener('click', function () {
+            // Solo contrae el menú si está en modo "show" (visible)
+            if (navbarCollapse.classList.contains('show')) {
+                toggleMenu();
+            }
+        });
     });
 });
-
-
-
